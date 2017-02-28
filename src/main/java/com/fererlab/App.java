00b0381bff26319e71eafc34e2b08a6ed580db6d
Can A. MOGOL <canmogol@gmail.com>
2017-02-28 14:47:12 +0300

    initial commit

---
+++ src/main/java/com/fererlab/App.java
@@ -0,0 +1,13 @@
+package com.fererlab;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
